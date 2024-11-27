77de001f45d9a7ae61f3ea5d504afabed9e9cf98
Maximiliano Paris <mparis@hotmail.cl>
2024-11-27 01:27:32 -0700

    fix: transition and navbar burguer fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import {BrowserRouter} from "react-router-dom";
 import 'react-bootstrap'
-import {Container, Nav, Navbar} from "react-bootstrap";
+import {Button, Container, Nav, Navbar} from "react-bootstrap";
 import { useTheme } from "./ThemeSetter.jsx";
 import {useEffect} from "react";
 
@@ -12,11 +12,14 @@ function App() {
     useEffect(() => {
         // Access the root element and set the background color
         const root = document.getElementById("root");
+        const body = document.body
         if (root) {
             if (theme === "light") {
                 root.style.backgroundColor = "#ffffff"; // Set the background color you want
+                body.style.backgroundColor = "#ffffff"; // Set the background color you want
             } else {
                 root.style.backgroundColor = "#000000"; // Set the background color you want
+                body.style.backgroundColor = "#000000"; // Set the background color you want
             }
         }
     }, [theme]) // Empty array ensures this runs once when the component mounts
@@ -25,41 +28,36 @@ function App() {
 
     return (
     <BrowserRouter >
-        <Navbar expand="lg" className="bg-gray" fixed="top">
+        <Navbar expand="lg" className={theme === "light" ? "bg-light" :"bg-gray"}  variant={theme === "light" ? "light" :"dark"} fixed="top">
             <Container>
-                <Navbar.Brand className={"text-white"} href="#home">MP</Navbar.Brand>
+                <Navbar.Brand className={theme === "light" ? "text-dark" : "text-white"} href="#home">MP</Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbar-content" />
                 <Navbar.Collapse id="navbar-content" className="justify-content-end">
                     <Nav >
                         <Nav.Item>
-                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link className={theme === "light" ? "text-dark" : "text-white"} href="#home">Home</Nav.Link>
                         </Nav.Item>
                         <Nav.Item>
-                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link className={theme === "light" ? "text-dark" : "text-white"} href="#home">Home</Nav.Link>
                         </Nav.Item>
                         <Nav.Item>
-                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link className={theme === "light" ? "text-dark" : "text-white"} href="#home">Home</Nav.Link>
                         </Nav.Item>
                         <Nav.Item>
-                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link className={theme === "light" ? "text-dark" : "text-white"} href="#home">Home</Nav.Link>
                         </Nav.Item>
-
+                        <Nav.Item>
+                            <Button onClick={toggleTheme}>
+                                {theme === "light" ? "Dark" : "Light"}
+                            </Button>
+                        </Nav.Item>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
 
         <Container className="mt-5">
-            <button onClick={toggleTheme}>
-                Switch to {theme === "light" ? "Dark" : "Light"} Theme
-            </button>
-            { theme === "light" ? (
-                <h1 className="text-black">Hello World!</h1>
-            ):(
-                <h1 className="text-white">Hello World!</h1>
-            )
-
-            }
+                <h1 className={ theme === "light" ? "text-black" : "text-white" } >Hello World!</h1>
         </Container>
 
     </BrowserRouter>
