e9b8f82e1ef16fbb5248f0983b642a26a5c1a8aa
Maximiliano Paris <mparis@hotmail.cl>
2025-04-12 23:05:28 -0600

    feat: byu project

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -7,12 +7,11 @@ import {ProjectsList} from "./ProjectsList.tsx";
 import {SkillsList} from "./SkillsList.tsx";
 
 export default function Home() {
-  const { getStyle } = useTheme();
   return (
    <>
      <Hero />
      <HomeObject title="Check Out My Recent Projects" children={<ProjectsList />} />
-     <HomeObject title="My Expertise and Skillset" children={<SkillsList/>}/>
+     {/*<HomeObject title="My Expertise and Skillset" children={<SkillsList/>}/>*/}
      <HomeObject title="What I Have Been Up To" children={<LatestCommits />} />
      <HomeObject title="Let's Connect" children={<ContactForm />} />
    </>
--- src/components/home/ProjectsList.tsx
+++ src/components/home/ProjectsList.tsx
@@ -12,33 +12,54 @@ export interface ProjectPreview {
 export function ProjectsList() {
   const projects: ProjectPreview[] = [
     {
+      title: "BYU app",
+      description: "At BYU, I develop and maintain iOS applications that improve the experience and accessibility for " +
+        "students and faculty. My work includes designing and implementing core features like the BYU Info/IT Support " +
+        "views and the Classes widget, as well as fixing key issues in the Home Screen. I led the development of the " +
+        "Home Screen search functionality and created an interactive Intro Tour to support the app’s redesign—enhancing " +
+        "both usability and onboarding.",
+      link: "http://example.com",
+      image: "/assets/picture/byu.png",
+    },
+    {
       title: "Cono Sur",
-      description: "Discover an app showcasing delicious recipes from Chile, Argentina, Peru, and Uruguay. " +
-        "Explore traditional dishes, learn about cultural flavors, and try your hand at preparing iconic " +
-        "meals from these South American countries.",
+      description: "A native iOS app showcasing traditional recipes from southern South America, including countries " +
+        "like Chile, Argentina, Paraguay, and Peru. Each recipe features an image (loaded via AsyncImage), along " +
+        "with key details such as servings, cook time, ingredients, and step-by-step instructions. Users can edit all " +
+        "recipe fields, including the title, description, instructions, and categories. Favorites can be saved, and users " +
+        "can create entirely new recipes and categories. The app uses SwiftData for local persistence and supports " +
+        "NavigationSplitView to deliver an optimized experience for iPad users.",
       link: "http://example.com",
       image: "/assets/picture/recipes.png",
     },
     {
       title: "FocusFlow",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et " +
-        "dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " +
-        "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat " +
-        "cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      description: "FocusFlow is a SwiftUI app designed to support individuals with ADHD and other neurodivergences " +
+        "in staying focused and organized. It guides users through structured Focus Sessions with real-time feedback on " +
+        "remaining time, upcoming tasks, and time saved by staying on track. Timers and sessions persist in the " +
+        "background for uninterrupted focus. Users receive timely alerts as tasks approach, reach, or exceed their " +
+        "time limits. The app also supports Live Activities, allowing users to monitor their sessions directly from the " +
+        "Lock Screen or Dynamic Island without opening the app.",
       link: "http://example.com",
       image: "/assets/picture/focusflow.png",
     },
     {
       title: "Improvify",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et " +
-        "dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ",
+      description: "Improvify is a minimalist SwiftUI habit tracker that helps users build positive routines without " +
+        "the pressure of perfect streaks. Instead of focusing on consecutive completions, Improvify highlights " +
+        "long-term progress through a dynamic graph that visualizes improvement over time. Users can create both " +
+        "daily and weekly habits, set smart reminders, and track their consistency without guilt—making the app ideal " +
+        "for personal growth, wellness, and productivity goals. Built using SwiftUI, SwiftData, and SwiftCharts. ",
       link: "http://example.com",
       image: "/assets/picture/improvify.png",
     },
     {
       title: "Learn Spanish",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et " +
-        "dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ",
+      description: "A SwiftUI language learning app designed to make vocabulary practice engaging and interactive. " +
+        "Each topic includes a lesson, practice mode, quiz mode, and a word list—unlocked progressively to simulate " +
+        "learning levels like Duolingo. Practice mode features animated, swipeable cards with color-coded definitions, " +
+        "while quiz mode offers time-based challenges, bonus points, sound feedback, and score tracking. User progress " +
+        "is persisted via UserDefaults, and the app supports both portrait and landscape orientations across devices.",
       link: "http://example.com",
       image: "/assets/picture/learnSpanish.png",
     }
